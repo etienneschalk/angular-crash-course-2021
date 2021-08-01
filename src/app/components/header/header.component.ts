ca7103b806641db76f5ac6a3286646eeebedbb08
Etienne Schalk <etienne.schalk@grenoble-inp.org>
2021-08-01 18:41:10 +0200

    Initial Commit - after watching tutorial

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title: string = 'Task Tracker';
+  showAddTask?: boolean = false;
+  subscription?: Subscription;
+
+  faPlus = faPlus;
+  faTimes = faTimes;
+
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleAddTask(): void {
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
+}
